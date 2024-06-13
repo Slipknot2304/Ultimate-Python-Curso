dc4a7a10c6c9cf3b0a87714d212d6bb430a84ed0
Armando Roldan <armando.zumpa@gmail.com>
2024-06-12 20:14:17 -0600

    Ejercicio cuatro

--- ejercicio.py
+++ ejercicio.py
@@ -14,3 +14,9 @@ print(f"{nombre_usuario.title()} {"\n"}")
 # Ejercicio tres
 n = len(nombre_usuario)
 print(f"{nombre_usuario.upper()} {n}")
+
+# Ejercicio cuatro
+telefono = input("Escribe tu telefono con prefijo y extencion (14 numeros):")
+
+print("Tu telefono sin prefijo ni extencion es este?:")
+print(telefono[2:12])
